ab71b0c3c7a50c27d3765cecff05c7c27fd78ae4
Matt Jones <matthew.jones@ga.co>
2018-03-18 19:36:48 -0700

    mario's website

--- js/main.js
+++ js/main.js
@@ -99,6 +99,55 @@ $('nav li:nth-child(6)').click(function() {
 })
 
 
+$('#filter li').hover(function() {
+    $( this ).addClass('active-filter-hover');
+  }, function() {
+    $( this ).removeClass('active-filter-hover');
+    $
+  }
+);
+
+$('#filter li').click(function() {
+	$('.active-filter .diamond-timeline').hide();
+	$('#filter li').removeClass('active-filter');
+	$(this).addClass('active-filter');
+	$('.active-filter .diamond-timeline').show();
+})
+
+$('.all-group').click(function() {
+	$('.project').show('slow');
+})
+
+$('.econometrics-group').click(function() {
+	$('.project').hide();
+	$('.econometrics').show('slow');
+})
+
+$('.geospatial-group').click(function() {
+	$('.project').hide();
+	$('.geospatial').show('slow');
+})
+
+$('.kmeans-group').click(function() {
+	$('.project').hide();
+	$('.kmeans').show('slow');
+})
+
+$('.pca-group').click(function() {
+	$('.project').hide();
+	$('.pca').show('slow');
+})
+
+$('.regularization-group').click(function() {
+	$('.project').hide();
+	$('.regularization').show('slow');
+})
+
+$('.timeseries-group').click(function() {
+	$('.project').hide();
+	$('.timeseries').show('slow');
+})
+
 
 
 
